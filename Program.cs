d28118eaca39d1c78673e1c60030d1312f9a7343
opilane <opilane@HP-CZC6297B89>
2021-06-11 11:04:45 +0300

    Добавил листы

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -12,23 +13,31 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
 
-            Draw(x1, y1, sym1);
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
 
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            numList.RemoveAt(0);
 
 
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
+
+            
+
 
 
         }
@@ -36,7 +45,7 @@ namespace Snake
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
-
+            Console.ReadLine();
         }
     }
 }
